1a305e7ba5ead1a6702f6b00754d9d468bf45d84
Christine Zedday <christine_bache@yahoo.fr>
2021-08-20 10:01:41 +0200

    lieux

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * Phototec
+ * Symfony 5
+ * Christine Zedday
+ */
+
+namespace App\Controller;
+
+use App\Service\Generator;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    
+}
